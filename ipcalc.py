fdfd995dabd0f9e90bcf4daf3ec8cf88d1b3e25a
David M. Johnson <johnsond@flux.utah.edu>
2023-05-06 21:33:23 -0600

    python3 fixes.

--- ipcalc.py
+++ ipcalc.py
@@ -1,17 +1,19 @@
 #!/usr/bin/env python
 
+from __future__ import print_function
+
 import sys
 
 def mask2bits(mask):
     ma = mask.split('.')
     if len(ma) != 4:
-        print "ERROR: malformed subnet mask %s" % (mask,)
+        print("ERROR: malformed subnet mask %s" % (mask,))
         sys.exit(1)
     last = 255
     for octet in ma:
         if not octet in ['255','254','252','248','240','224','192','128','0'] \
           or int(octet) > last:
-            print "ERROR: malformed subnet mask %s" % (mask,)
+            print("ERROR: malformed subnet mask %s" % (mask,))
             sys.exit(1)
             pass
         last = int(octet)
@@ -26,18 +28,18 @@ def mask2bits(mask):
             octet >>= 1
             pass
         pass
-    print str(bits)
+    print(str(bits))
     return 0
 
 if __name__ == '__main__':
-    if sys.argv < 3:
-        print "ERROR: must supply at least three arguments!"
+    if len(sys.argv) < 3:
+        print("ERROR: must supply at least three arguments!")
         sys.exit(1)
         pass
     if sys.argv[1] == 'mask2bits':
         ret = mask2bits(sys.argv[2])
     else:
-        print "ERROR: unsupported subcommand!"
+        print("ERROR: unsupported subcommand!")
         sys.exit(1)
         pass
     sys.exit(ret)
